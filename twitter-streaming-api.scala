b70484e66cbeb9e3e03527a08b20ad9aaeb4bde0
Mohammed Rhaouti <31079981+XRotoX@users.noreply.github.com>
2021-12-13 12:15:30 +0100

    Comment added

--- twitter-streaming-api.scala
+++ twitter-streaming-api.scala
@@ -6,6 +6,8 @@ import org.apache.spark.streaming.twitter._
  
 object PrintTweets {
    def main(args: Array[String]) {  
+       // These credentials are revoked please consider using your own
+       // Visit https://developer.twitter.com for more
        System.setProperty("twitter4j.oauth.consumerKey", "N5IWiD7jWztbO6sktS0zr6Ct8")
        System.setProperty("twitter4j.oauth.consumerSecret", "3xBUQw9N2dWRrW4XxSyzlmI8MNS3AXG8Nsy3pdMnczUWsrIlwA")
        System.setProperty("twitter4j.oauth.accessToken", "1357063042945712139-2glVWIqkAIBRuUP0S7DnVvUsphxSPe")
